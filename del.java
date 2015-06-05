a45a58024ee20a2a2f0e49490d14600b0f6551c7
sreehari.v <sreehari.vv007@gmail.com>
2015-06-05 12:16:42 +0530

    New changelist

---
+++ del.java
@@ -0,0 +1,24 @@
+/**
+ * Created by sreehari.v on 20/05/15.
+ */
+public class del
+{   int a;
+    int b;
+    public  int adder ()
+    {
+
+        return a+b;
+    }
+
+public int mul ()
+{
+    return a*b;
+
+}
+
+    public void  newfn()
+    {
+        int sam;
+
+    }
+}
