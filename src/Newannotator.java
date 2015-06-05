18fad075f2315b3db8a8b60c9fc976b68b820da1
sreehari.v <sreehari.vv007@gmail.com>
2015-06-05 12:39:24 +0530

    commiting

---
+++ src/Newannotator.java
@@ -0,0 +1,11 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Created by sreehari.v on 14/05/15.
+ */
+public class Newannotator extends AnAction {
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
